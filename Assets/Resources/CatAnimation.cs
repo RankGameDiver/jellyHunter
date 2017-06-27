ffe5d0f042305cf560867de81c22c32cd0dd181a
RankGameDiver <hoo2018@naver.com>
2017-06-27 20:44:19 +0900

    animation fix

--- Assets/Resources/CatAnimation.cs
+++ Assets/Resources/CatAnimation.cs
@@ -16,30 +16,34 @@ public class CatAnimation : MonoBehaviour
     void Update()
     {
         if (GameData.checkTouchblock)
-            animation1();
+            animations();
         else if (GameData.skillKind == 0)
         {
             animator.SetInteger("Status", 0);
         }
+        else
+        {
+            GameData.skillKind = 0;
+        }
     }
 
-    private void animation1()
+    private void animations()
     {
         switch (GameData.skillKind) // GameData.skillKind: 스킬블럭 상태 받아오는 거 표현한 임시변수
         {
             case 1:
                 animator.SetInteger("Status", 1);
-                GameData.skillKind = 0;
+                //GameData.skillKind = 0;
                 Debug.Log("skillNum_1");
                 break;
             case 2:
                 animator.SetInteger("Status", 2);
-                GameData.skillKind = 0;
+                //GameData.skillKind = 0;
                 Debug.Log("skillNum_2");
                 break;
             case 3:
                 animator.SetInteger("Status", 3);
-                GameData.skillKind = 0;
+                //GameData.skillKind = 0;
                 Debug.Log("skillNum_3");
                 break;
             default:
