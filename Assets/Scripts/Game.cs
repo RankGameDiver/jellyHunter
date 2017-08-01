be6dce44174c1936430e22408eab066b9b525933
RankGameDiver <hoo2018@naver.com>
2017-08-01 16:47:00 +0900

    fix

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     public GameObject[] sBlock;
+    public Block[] cBlock;
     public GameObject currentBlock;
 
     void Start()
@@ -18,13 +19,27 @@ public class Game : MonoBehaviour
 
     public void OnAct() // 오브젝트 활성화
     {
-        gameObject.SetActive(true);
+        for (int i = 0; i < GameData.blockAmount; i++)
+        {
+            if (!sBlock[i].activeInHierarchy)
+            {
+                sBlock[i].SetActive(true);
+                sBlock[i].transform.position = (GameData.spawnPos);
+                currentBlock = sBlock[i] ;
+                return;
+            }
+        }
     }
 
     public void OffAct() // 오브젝트 비활성화
     {
         gameObject.SetActive(false);
         GameData.blockCount--;
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(gameObject);
     }
 
     IEnumerator LogicLoop()
@@ -39,13 +54,16 @@ public class Game : MonoBehaviour
     {
         if (GameData.blockCount < 7)
         {
-            sBlock[GameData.blockCount] = currentBlock;
-            
-            Block block = currentBlock.GetComponent<Block>();
+            OnAct();
 
-            block.MoveBlock(GameData.blockCount);
-
-            yield return new WaitUntil(() => { return !block.isMoving; });//*
+            for (int i = 0; i < 7; i++)
+            {
+                if (currentBlock == sBlock[i])
+                {
+                    cBlock[i].MoveBlock(GameData.blockCount);
+                    yield return new WaitUntil(() => { return !cBlock[i].isMoving; });//*
+                }
+            }
         }
 
         yield break;    //코루틴 종료시키는 코드
