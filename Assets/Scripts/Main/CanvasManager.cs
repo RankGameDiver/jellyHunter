af475b7b3fbdce08a64f95af20250d48c2036b72
RankGameDiver <hoo2018@naver.com>
2017-12-05 19:59:42 +0900

    UImanager

---
+++ Assets/Scripts/Main/CanvasManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    public Canvas canvas;
+    private GameObject saveButton;
+    private GameObject loadButton;
+
+    void Awake()
+    {
+        SetButton();
+    }
+
+    /*transform.Find("오브젝트 이름"); // 자식이나 부모 관계 안에서만 찾음
+    GameObject.Find("오브젝트 이름"); // Hierarchy안에서 다 찾음
+    GameObject.FindWithTag("태그 이름"); // 해당하는 태그에 속하는 오브젝트 반환. 여러 개일 경우 그 중에 랜덤으로 반환*/
+
+    private void SetButton()
+    {
+        saveButton = GameObject.Find("Save");
+        loadButton = GameObject.Find("Load");
+        RectTransform saveRect = saveButton.GetComponent<RectTransform>();
+        saveRect.anchoredPosition = new Vector2(200,-100);
+        RectTransform loadRect = loadButton.GetComponent<RectTransform>();
+        loadRect.anchoredPosition = new Vector2(-200, -100);
+    }
+}
---
+++ Assets/Scripts/Main/UImanager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UImanager : MonoBehaviour
+{
+
+    public Canvas canvas;
+    private GameObject[] window;
+    private GameObject[] button;
+    private GameObject shop;
+    private GameObject stageSelect;
+    public int scale;
+
+    void Awake()
+    {
+        window = new GameObject[3];
+        button = new GameObject[3];
+        SetUI();
+    }
+
+    private void SetUI()
+    {
+        window[0] = GameObject.Find("diaWindow");
+        window[1] = GameObject.Find("jellyWindow");
+        window[2] = GameObject.Find("lifeWindow");
+
+        button[0] = GameObject.Find("diaButton");
+        button[1] = GameObject.Find("jellyButton");
+        button[2] = GameObject.Find("lifeButton");
+
+        shop = GameObject.Find("Shop");
+        stageSelect = GameObject.Find("StageSelect");
+
+        window[0].GetComponent<RectTransform>().anchoredPosition = new Vector2(-1750, -138);
+        window[1].GetComponent<RectTransform>().anchoredPosition = new Vector2(-1194, -135);
+        window[2].GetComponent<RectTransform>().anchoredPosition = new Vector2(-439, -124);
+
+        button[0].GetComponent<RectTransform>().anchoredPosition = new Vector2(-1534, -134);
+        button[1].GetComponent<RectTransform>().anchoredPosition = new Vector2(-1002, -134);
+        button[2].GetComponent<RectTransform>().anchoredPosition = new Vector2(-104, -134);
+
+        shop.GetComponent<RectTransform>().anchoredPosition = new Vector2(488, -96);
+        stageSelect.GetComponent<RectTransform>().anchoredPosition = new Vector2(-255, 384);
+    }
+}
