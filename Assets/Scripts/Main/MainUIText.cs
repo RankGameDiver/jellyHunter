56052b533f9439f23bd97fdc931d3747aa92ffca
JENNU4746 <jennu4746@icloud.com>
2018-01-06 22:15:44 +0900

    Added Main Scene UI Text

---
+++ Assets/Scripts/Main/MainUIText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUIText : MonoBehaviour {
+
+    public Text moneyT;
+
+    void Awake()
+    {
+        moneyT = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyT.text = ScoreManager.money.ToString();
+    }
+}
