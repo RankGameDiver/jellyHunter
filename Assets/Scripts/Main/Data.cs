9ca1439a8c31903e33cd3ce58c27c28d38948c2c
RankGameDiver <hoo2018@naver.com>
2018-01-07 10:06:15 +0900

    bug fix

---
+++ Assets/Scripts/Main/Data.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    public int money;
+
+    public Data(int t_money)
+    {
+        money = t_money;
+    }
+}
