c38916d1277b2689f707b2d38eda0972c8b65cec
RankGameDiver <hoo2018@naver.com>
2018-01-15 13:49:30 +0900

    Shop Scroll view finish

---
+++ Assets/Scripts/Main/Shop.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    private int itemKind;
+    private RectTransform shopPos;
+
+    void Start()
+    {
+        shopPos = GetComponent<RectTransform>();
+        shopPos.anchoredPosition = new Vector3(0, 0, 100);
+    }
+
+    public void Buy()
+    {
+        switch (itemKind)
+        {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+    }
+}
