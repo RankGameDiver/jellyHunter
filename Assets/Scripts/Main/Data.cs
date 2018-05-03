8f790544f9b2ef8da6c005166c7016d4513bbcfa
RankGameDiver <hoo2018@naver.com>
2018-05-03 17:18:32 +0900

    fix

--- Assets/Scripts/Main/Data.cs
+++ Assets/Scripts/Main/Data.cs
@@ -1,16 +1,14 @@
-﻿
-
-public class Data
+﻿public class Data
 {
     public int money;
+    public int playingCount;
+    public int sTime;
+
     public int stageT;
     public int stage1;
     public int stage2;
     public int stage3;
     public int exStage;
-
-    public int playingCount;
-    public int sTime;
 
     public int attackUp;
     public int defendUp;
@@ -20,13 +18,15 @@ public class Data
         int _exStage = 0, int _playingCount = 0, int _sTime = 0, int _attackUp = 0, int _defendUp = 0, int _healthUp = 0)
     {
         money = _money;
+        playingCount = _playingCount;
+        sTime = _sTime;
+
         stageT = _stageT;
         stage1 = _stage1;
         stage2 = _stage2;
         stage3 = _stage3;
         exStage = _exStage;
-        playingCount = _playingCount;
-        sTime = _sTime;
+
         attackUp = _attackUp;
         defendUp = _defendUp;
         healthUp = _healthUp;
--- Assets/Scripts/Main/SaveLoad.cs
+++ Assets/Scripts/Main/SaveLoad.cs
@@ -39,13 +39,13 @@ public class SaveLoad : MonoBehaviour
     public void GetData(JsonData data)
     {
         GameData.Money = (int)data[0]["money"];
+        GameData.PlayingCount = (int)data[0]["playingCount"];
+        GameData.sTime = (int)data[0]["sTime"];
         GameData.StageT = (int)data[0]["stageT"];
         GameData.Stage1 = (int)data[0]["stage1"];
         GameData.Stage2 = (int)data[0]["stage2"];
         GameData.Stage3 = (int)data[0]["stage3"];
         GameData.ExStage = (int)data[0]["exStage"];
-        GameData.PlayingCount = (int)data[0]["playingCount"];
-        GameData.sTime = (int)data[0]["sTime"];
         GameData.attackUp = (int)data[0]["attackUp"];
         GameData.defendUp = (int)data[0]["defendUp"];
         GameData.healthUp = (int)data[0]["healthUp"];
